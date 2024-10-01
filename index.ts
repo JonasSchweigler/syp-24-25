e2e3a302868d483b7e14ba980672388a2ccf503b
JonasSchweigler <swejoz19@htl-kaindorf.at>
2024-10-01 14:36:39 +0200

    setup repo

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import ytdl from "ytdl-core";
+import { google } from "googleapis";
+import contentDisposition from "content-disposition";
+import statisticRoutes from "./routes";
+import dotenv from "dotenv";
+import { sendMail } from "./sendMail";
+dotenv.config();
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
