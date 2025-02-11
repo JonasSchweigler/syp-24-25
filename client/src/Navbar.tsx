b8825fa9495c1ef9750ecb6b5c6036a238ea0531
JonasSchweigler <swejoz19@htl-kaindorf.at>
2025-02-11 15:37:38 +0100

    changed navbar title

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
                 href={`/`}
                 _hover={{ textDecoration: "none", color: "gray.500" }}
               >
-                Petersquelle
+                Test
               </Link>
             </Heading>
           </HStack>
